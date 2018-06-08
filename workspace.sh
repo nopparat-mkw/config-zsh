89e6ae3ad1bb908403b9da2bea35d763c6daca96
nopparat <nopparat.mueangkaew@gmail.com>
2018-06-08 10:14:49 +0700

    Add docker command

--- workspace.sh
+++ workspace.sh
@@ -62,4 +62,41 @@ pg() {
 alias cls="clear"
 alias ys="yarn start"
 alias yi="yarn"
+alias yl="yarn lint"
 alias mvn_dev="mvn spring-boot:run -Drun.arguments='spring.profiles.active=develop'"
+
+# backend 
+bg() {
+  cd "$HOME/project/9PiProjectBackend/$1" || return
+}
+
+# frontend
+fg() {
+  cd "$HOME/project/9PiProjectFrontend/$1" || return
+}
+
+# bsh: Shell into the Backend
+bsh() {
+  docker exec -it "$1" bash
+}
+
+# br: Run the Backend
+br() {
+  (bg $1 && docker-compose up -d)
+}
+
+# bk: Kill the Backend
+bk() {
+  (bg $1 && docker-compose down)
+}
+
+# bs: Stop the Backend
+bs() {
+  (bg $1 && docker-compose stop)
+}
+
+# Lazy alias for `docker-compose up`
+alias du="docker-compose up"
+alias dps="docker ps"
+alias dpsa="docker ps -a -s"
+alias di="docker images"
